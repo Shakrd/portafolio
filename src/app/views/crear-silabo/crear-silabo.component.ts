585a2f56fc449bbfb18f3ab6732f9d9851c2ed91
Shakrd <jvc.vega@yavirac.edu.ec>
2020-02-28 10:14:52 -0500

    Coorrequisitos y Prerrequisitos

--- src/app/views/crear-silabo/crear-silabo.component.ts
+++ src/app/views/crear-silabo/crear-silabo.component.ts
@@ -13,11 +13,31 @@ export class CrearSilaboComponent {
   {id:4, nombre: 'Tendencias de Programación, 2019-2020'}
 ]
 
+correquisitosAsignatura: any = [
+  {id:1, nombre: 'Base de Datos, 2019-2020', codigo: 'abc1234'},
+  {id:2, nombre: 'Ingeniería de Software, 2019-2020', codigo: 'qwer789'}
+ 
+]
+
+prerrequisitosAsignatura: any = [
+  {id:1, nombre: 'Base de Datos, 2019-2020', codigo: 'abc1234'},
+  {id:2, nombre: 'Ingeniería de Software, 2019-2020', codigo: 'qwer789'},
+  {id:3, nombre: 'Procesos, 2019-2020', codigo: 'pjyge8925'},    
+  {id:4, nombre: 'Tendencias de Programación, 2019-2020', codigo: 'azxc463'}
+]
   constructor(private formBuilder: FormBuilder) {}
     
   asignaturasForm = this.formBuilder.group({
     asignaturas: ['']
   }) 
+
+  correquisitosForm = this.formBuilder.group({
+    correquisitosAsignatura: ['']
+  })
+
+  prerrequisitosForm = this.formBuilder.group({
+    correquisitosAsignatura: ['']
+  })
 
     submit() {
       alert(JSON.stringify(this.asignaturasForm.value))
